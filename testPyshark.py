cb18a5bb190549fb12b95d0b38bf6b03e6166179
Daniel Quackenbush <TheQuack45@gmail.com>
2022-11-14 02:16:31 -0500

    Started a Pyshark test script.
    Just playing around with it to learn the API at this point. It is not
    integrated with the main Selenium test yet, nor does it collect all the
    right information.

---
+++ testPyshark.py
@@ -0,0 +1,44 @@
+import pyshark
+from pyshark import LiveCapture
+
+INTERFACE = "Wi-Fi"
+
+class NonPromLiveCapture(LiveCapture):
+    def get_parameters(self, packet_count=None):
+        params = super(LiveCapture, self).get_parameters(packet_count = packet_count)
+        for interface in self.interfaces:
+            params += ["-i", interface]
+        if self.bpf_filter:
+            params += ["-f", self.bpf_filter]
+        params += ["-p"] # Disable promiscuous mode.
+        return params
+
+def main():
+    capture = NonPromLiveCapture(interface = INTERFACE)
+    print("Sniffing")
+    capture.sniff(packet_count = 50)
+    print(capture)
+    ct = 0
+    for packet in capture:
+        #print(dir(packet))
+
+        layer = getattr(packet, "tcp", None)
+        if layer is not None:
+            print("Packet is TCP")
+            #print(dir(layer))
+            print_packet_time(layer)
+            #break
+        
+        layer = getattr(packet, "udp", None)
+        if layer is not None:
+            print("Packet is UDP")
+            #print(dir(layer))
+            print_packet_time(layer)
+            #break
+
+def print_packet_time(transport_layer):
+    #print("time_delta: " + str(transport_layer.time_delta))
+    print("time_relative: " + str(transport_layer.time_relative))
+
+if __name__ == "__main__":
+    main()
