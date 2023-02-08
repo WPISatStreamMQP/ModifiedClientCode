61ec45bd8f2512e488b51f568ee7813f1dcfc88b
Employee_NO427 <zijian.guan@outlook.com>
2023-02-08 17:36:13 -0500

    move test script from colab to python as is without changes

--- script.js
+++ script.js
@@ -153,6 +153,10 @@ function display(){
     var player = dashjs.MediaPlayer().create();
     player.updateSettings({
         streaming: {
+        abr:{
+            useDefaultABRRules:true,
+            ABRStrategy:"abrThroughput"
+        },
         buffer:{
             bufferToKeep: 20, // Default
             stableBufferTime: 20, // Default: 12
