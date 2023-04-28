96ae1e96372c7fd2ee9c4bc495fb764e0a5fd328
kristiprifti <61074990+kristiprifti@users.noreply.github.com>
2023-04-28 13:39:38 -0400

    Last Commit
    This code goes into every directory, analyzes the files, generates the plots and put the aggregated results in a new directory for further analysis

--- analysis_plots.py
+++ analysis_plots.py
@@ -1,3 +1,7 @@
+# This code goes into every directory analyzes the files, generates
+# the plots and put the aggregated csv results in a new directory for further analysis
+
+
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
@@ -5,8 +9,10 @@ import pandas as pd
 import re
 import math
 import os
+import plotly.io as pio
 
 
+# This function plots the stalls over time. Spikes are the stalls
 def get_time_elapsed_stall_mapping(filename):
     x = []
     y = []
@@ -16,7 +22,6 @@ def get_time_elapsed_stall_mapping(filename):
         lines = f.readlines()
         for line in lines:
             row_element = line.strip().split()
-            # ['LOG', 0.423....]
             if row_element[0] == 'START':
                 continue
             if row_element[0] == 'QUAL':
@@ -27,14 +32,13 @@ def get_time_elapsed_stall_mapping(filename):
                 y.append(float(row_element[1]))
                 x.append(previous_time)
                 continue
-            log_details = row_element[1] #
+            log_details = row_element[1]
             log_elements = log_details.split(",")
             x.append(float(log_elements[0]))
             y.append(0)
             y_dev = np.divide(y, myInt)  # convert STALLS from ms to sec
             y_sec = [round(x, 1) for x in y_dev]
             previous_time = float(log_elements[0])
-
 
     df = pd.DataFrame({'x': x, 'y': y_sec})
 
@@ -50,35 +54,47 @@ def get_time_elapsed_stall_mapping(filename):
         fig1 = px.bar(describe_num_df, x="index", y=i, text=i, labels={
             "x": "Statistics",
             "y": "Stalls(sec)",
-        }, title="Stalls Statistics")
-        #fig1.show()
-    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_Statistics_.html'.format(filename))
-    fig1.write_html(path_Name)
+        })
+        fig1.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+    # path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_Statistics_.html'.format(filename))
+    # fig1.write_html(path_Name)
+    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_Statistics_.svg'.format(filename))
+    fig1.write_image(path_Name, format='svg')
 
     text_in = []
     for index in range(len(x)):
         text_in.append(y_sec[index])
 
-    fig2 = px.line(df, x='x', y='y',labels={
+    fig2 = px.line(df, x='x', y='y', labels={
         "x": "Time (sec)",
         "y": "Stalls(sec)",
-    }, title="Stalls (sec) / Time(sec) for: {}".format(filename))
+    })
+    fig2.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig2.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig2.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
     fig2.update_traces(textposition="top right")
 
-    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_.html'.format(filename))
-    fig2.write_html(path_Name)
+    # path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_.html'.format(filename))
+    # fig2.write_html(path_Name)
+    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_.svg'.format(filename))
+    fig2.write_image(path_Name, format='svg')
 
     # delete all 0 values from the dataframe
     df = df[df.y != 0]
     # whisker plot
-    fig3 = px.box(df, y="y", labels={   "y": "Stalls(sec)",}, title="Stalls (sec) / Time(sec) for: {}".format(filename))
-    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_whisker_.html'.format(filename))
-    fig3.write_html(path_Name)
+    fig3 = px.box(df, y="y", labels={"y": "Stalls(sec)",})
+    fig3.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig3.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig3.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+    # path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_whisker_.html'.format(filename))
+    # fig3.write_html(path_Name)
+    path_Name = os.path.join(os.getcwd(), 'Stall_Results', '{}_resultStallsResults_whisker_.svg'.format(filename))
+    fig3.write_image(path_Name, format='svg')
 
 
     return pd.DataFrame({'x': x, 'y': y_sec})
 
-
+# This function builds a table of diffrenet  statistics
 def calculate_table(filename):
     # Getting input
     path_name = os.path.join(os.getcwd(), filename)
@@ -94,21 +110,17 @@ def calculate_table(filename):
     # Generate figure
     summary_fig = go.Figure(data=[go.Table(
         header=dict(values=list(summary_df.reset_index().columns),
-                    fill_color='paleturquoise',
                     align='left'),
         cells=dict(values=summary_df.reset_index().T,
-                   fill_color='lavender',
                    align='left'))
     ])
-    summary_fig.update_layout(title="Table Summary Statistics for: {}".format(filename))
-    path_Name = os.path.join(os.getcwd(), 'Table_Results', '{}_Table_.html'.format(filename))
-    summary_fig.write_html(path_Name)
+    summary_fig.update_layout(title=None, plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+
+    path_Name = os.path.join(os.getcwd(), 'Table_Results', '{}_Table_.svg'.format(filename))
+    summary_fig.write_image(path_Name, format='svg')
 
 
-
-
-
-
+# This function is used to generate the graph for the resolution over time graph
 def get_time_elapsed_qual_mapping(filename):
     x = []
     y = []
@@ -133,81 +145,110 @@ def get_time_elapsed_qual_mapping(filename):
     fig = px.line(df, x, y, labels={
         "x": "Time",
         "y": "Resolution",
-    }, title="Resolution vs Time for: {}".format(filename))
+    })
+    fig.update_layout(title=None, plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
     fig.update_yaxes(categoryorder='array',
                      categoryarray=['480x270', '640x360', '960x540','1280x720','1920x1080','3840x2160'])
-    path_Name = os.path.join(os.getcwd(), 'Qual_Results', '{}_resultQual_Time_.html'.format(filename))
-    fig.write_html(path_Name)
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    path_Name = os.path.join(os.getcwd(), 'Qual_Results', '{}_resultQual_Time_.svg'.format(filename))
+    fig.write_image(path_Name, format='svg')
 
     return pd.DataFrame({'x': x, 'y': y})
 
 
-
+# This function is used to get the time elapsed and the size of the buffer
 def analyze_size_of_buffer(log_file):
-    path_name = os.path.join(os.getcwd(),log_file)
+    path_name = os.path.join(os.getcwd(), log_file)
 
     df = pd.read_csv(path_name, sep="\\n", names=['Params'])
 
-    log_df = df["Params"].str.extract(r"LOG  (?P<realtime_elapsed>\d+\.?\d*),(?P<current_time>\d+\.?\d*):(?P<current_fps>\d*),(?P<current_horizontal_resolution>\d+)x(?P<cur_vert_res>\d*),(?P<current_bitrate>\d+),(?P<current_size_of_buffer>\d+\.?\d*)")
-    # log_df["current_size_of_buffer"]=pd.to_numeric(log_df["current_size_of_buffer"])
+    log_df = df["Params"].str.extract(
+        r"LOG  (?P<realtime_elapsed>\d+\.?\d*),(?P<current_time>\d+\.?\d*):(?P<current_fps>\d*),(?P<current_horizontal_resolution>\d+)x(?P<cur_vert_res>\d*),(?P<current_bitrate>\d+),(?P<current_size_of_buffer>\d+\.?\d*)")
     log_df = log_df.apply(pd.to_numeric)
-    # log_df["realtime_elapsed"]=pd.to_numeric(log_df["realtime_elapsed"])
     log_df = log_df.dropna()
-    fig = px.line(log_df, x="realtime_elapsed", y="current_size_of_buffer", title='realtime_elapsed vs current_size_of_buffer').update_layout(
-        xaxis_title="realtime_elapsed(Sec)", yaxis_title="current_size_of_buffer(Sec)",xaxis_tickformat=',d'
+
+    fig = px.line(log_df, x="realtime_elapsed", y="current_size_of_buffer")
+    fig.update_layout(
+        title=None,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis_title="Time(Sec)",
+        yaxis_title="Buffer Size(Sec)",
+        xaxis_tickformat=',d'
     )
-    path_Name = os.path.join(os.getcwd(), 'Size_of_Buffer_Results', '{}_Size_of_Buffer_Results_.html'.format(log_file))
-    fig.write_html(path_Name)
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+
+    path_Name = os.path.join(os.getcwd(), 'Size_of_Buffer_Results', '{}_Size_of_Buffer_Results_.svg'.format(log_file))
+    fig.write_image(path_Name, format='svg')
 
     return pd.DataFrame({'x': log_df["realtime_elapsed"], 'y': log_df["current_size_of_buffer"]})
 
-#  function for analyzing UDPing logs
+# This function is used to analyze the logs of UDPing
 def analyze_UDPing_logs(log_file):
     df = pd.read_csv(log_file, skiprows=1)
     server_list = df['server'].tolist()
     ping_statistics_row = server_list.index('Ping statistics ')
-    # split the dataframe into two dataframes
+
     df1 = df.iloc[:ping_statistics_row]
     df2 = df.iloc[ping_statistics_row:]
     df1['tSent(ms) '] = df1['tSent(ms) '].apply(lambda x: x / 1000)
-    # Plot the RTT vs Time
-    fig = px.scatter(df1, x='tSent(ms) ', y='rtt(ms)', title='UDPing RTT vs Time for: {}'.format(log_file))
-    fig.update_xaxes(title_text="Time sent (sec)")
-    fig.update_yaxes(title_text="RTT(sec)")
-    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_.html'.format(log_file))
-    fig.write_html(path_Name)
-    # Plot the summary statistics
+
+    fig = px.scatter(df1, x='tSent(ms) ', y='rtt(ms)')
+    fig.update_layout(
+        title=None,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis_title="Time sent (sec)",
+        yaxis_title="RTT(sec)"
+    )
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+
+    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_.svg'.format(log_file))
+    fig.write_image(path_Name, format='svg')
+
     df_summary = df1.describe()
     df_summary.drop(['bytes'], axis=1, inplace=True)
     df_summary.drop(['seq'], axis=1, inplace=True)
     df_summary.drop(['tSent(ms) '], axis=1, inplace=True)
     df_summary.drop(['count'], axis=0, inplace=True)
-    fig = px.bar(df_summary, title="RTT(ms) Statistics for: {}".format(log_file))
-    fig.update_xaxes(title_text="Statistics")
-    fig.update_yaxes(title_text="RTT(sec)")
-    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_Summary_.html'.format(log_file))
-    fig.write_html(path_Name)
-    # whisker plot for RTT values in UDPing logs
-    fig = px.box(data_frame=df1, x="rtt(ms)", orientation="h",
-                 title="RTT(sec) Statistics from UDPing for: {}".format(log_file))
-    fig.update_xaxes(title_text="RTT(sec)")
-    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_Whisker_.html'.format(log_file))
-    fig.write_html(path_Name)
 
-    # save df2 as csv
+    fig = px.bar(df_summary)
+    fig.update_layout(
+        title=None,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis_title="Statistics",
+        yaxis_title="RTT(sec)"
+    )
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_Summary_.svg'.format(log_file))
+    fig.write_image(path_Name, format='svg')
+
+    fig = px.box(data_frame=df1, x="rtt(ms)", orientation="h")
+    fig.update_layout(
+        title=None,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis_title="RTT(sec)"
+    )
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_Whisker_.svg'.format(log_file))
+    fig.write_image(path_Name, format='svg')
+
     path_Name = os.path.join(os.getcwd(), 'UDPing_Results', '{}_UDPing_Results_Summary.csv'.format(log_file))
     df2.to_csv(path_Name, index=False)
 
     return pd.DataFrame({'x': df1['tSent(ms) '], 'y': df1['rtt(ms)']})
 
+
 # function for calculating the throughput
 def analyze_packets(filtered_packets):
-
-    # read only the first 2 columns
     df_new = pd.read_csv(filtered_packets, usecols=[0, 1])
-
-    """# Preprocessing"""
-
 
     def grouping_attr(index):
         return math.floor(df_new['_ws.col.Time'].loc[index])
@@ -215,22 +256,30 @@ def analyze_packets(filtered_packets):
     df_new = df_new.groupby(grouping_attr)['frame.len'].sum().reset_index()
     df_new['frame.len'] = df_new['frame.len'].multiply(8e-6)  # multiply to get Mbit/s
 
-    fig2 = px.line(df_new,x="index",y="frame.len",
-            labels={
-                "index":"Time(s)",
-                "frame.len":"bitrate(Mbit/s)"
-            }
-               ,title='Throughput')
-    fig2.update_layout(showlegend=False)
-    fig2.update_layout(title_text='Throughput for: {}'.format(filtered_packets))
+    fig2 = px.line(df_new, x="index", y="frame.len",
+                   labels={
+                       "index": "Time(s)",
+                       "frame.len": "bitrate(Mbit/s)"
+                   }
+                  )
+    fig2.update_layout(
+        title=None,
+        showlegend=False,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis_title="Time(s)",
+        yaxis_title="Bitrate(Mbit/s)"
+    )
+    fig2.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig2.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
 
-    path_Name = os.path.join(os.getcwd(), 'Throughput_Results', '{}_Throughput_Results_.html'.format(filtered_packets))
-    fig2.write_html(path_Name)
+    path_Name = os.path.join(os.getcwd(), 'Throughput_Results', '{}_Throughput_Results_.svg'.format(filtered_packets))
+    fig2.write_image(path_Name, format='svg')
 
     return pd.DataFrame({'x': df_new['index'], 'y': df_new['frame.len']})
 
+
 if __name__ == "__main__":
-    # get all the files in the current directory and analyze them
     empty_files = []
     all_stalls_df = pd.DataFrame(columns=['x', 'y'])
     all_qual_map = pd.DataFrame(columns=['x', 'y'])
@@ -285,14 +334,13 @@ if __name__ == "__main__":
                 print("Done analyzing " + file)
 
 
-# create a new directory to store all the results
+# new directory to store all the results
 current_dir = os.getcwd()
 new_dir = os.path.join(current_dir, "All_Results")
 if not os.path.exists(new_dir):
     os.makedirs(new_dir)
 print("--" * 50)
 
-# delete all the files in the directory if they exist and create new ones with the same name and write the dataframes to them
 for file in os.listdir(new_dir):
     if file.startswith("All") and file.endswith(".csv"):
         print("File", file, "already exists, deleting it...")
